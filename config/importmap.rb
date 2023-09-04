ff1f5623193c82dd42b334ec9027e93611fbaf92
Phuc Thai <phuc.tm4192@gmail.com>
2023-09-04 16:06:17 +0700

    Add Bootstrap 5.3

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,6 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+
+pin "popper", to: 'popper.js', preload: true
+pin "bootstrap", to: 'bootstrap.min.js', preload: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "articles#index"
+  root "homes#index"
 
   resources :articles do
     resources :comments
